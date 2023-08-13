a5eb467f5c428cd2246dbc4ea45bae392fa82f11
Mike <mikemakol45@gmail.com>
2023-08-13 13:50:16 +0300

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,19 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  # def first_name
+  #   student = Student.find(params[id: :first_name])
+  #   render json: student
+  # end
+
+  # def last_name
+  #   student = Student.find(params[id: :last_name])
+  #   render json: student
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students/:id', to: 'students#show'
+  # get '/students/:id/:first_name', to: 'students#first_name'
+  # get '/students/:last_name', to: 'students#last_name'
 end
